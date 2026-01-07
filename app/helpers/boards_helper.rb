dc9c5444085e246f04ed5f5e31d9d2245152de89
Denis Švara <denis.svara@gmail.com>
2026-01-07 12:54:04 +0100

    Use bridge nav-button for board settings action.

--- app/helpers/boards_helper.rb
+++ app/helpers/boards_helper.rb
@@ -4,7 +4,11 @@ module BoardsHelper
   end
 
   def link_to_edit_board(board)
-    link_to edit_board_path(board), class: "btn", data: { controller: "tooltip" } do
+    link_to edit_board_path(board), class: "btn", data: {
+      controller: "tooltip bridge--nav-button",
+      bridge_icon_name: "gearshape.fill",
+      bridge_title: "Settings"
+    } do
       icon_tag("settings") + tag.span("Settings for #{board.name}", class: "for-screen-reader")
     end
   end
